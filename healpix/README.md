6cd450a1b4efccc84a4277a18dea93ca48d993d9
Tom Verbeure <hombre+github@gmail.com>
2019-09-07 14:26:55 -0700

    Sphere with healpix pixels

--- healpix/README.md
+++ healpix/README.md
@@ -6,5 +6,11 @@ https://healpix.sourceforge.io/index.php
 Download: https://healpix.sourceforge.io/downloads.php
 
 ```
+python3 -m venv pyvm
+source ./pyvm/bin/activate
+pip install --upgrade pip
+pip install solidpython
+
 tar xfvz ./ftp/Healpix_3.50_2018Dec10.tar.gz
+make all
 ```
--- healpix/src/healball.c
+++ healpix/src/healball.c
@@ -14,5 +14,28 @@ int main(int argc, char **argv)
 
     int npix = nside2npix(nside);
 
-    printf("npix: %d\n", npix);
+    float dot_radius = 0.5;
+    float sphere_radius = 5;
+
+    //printf("npix: %d\n", npix);
+
+    printf("from solid import *\n\n");
+
+    for(int i=0;i<npix;++i){
+        double vec[3];
+        pix2vec_ring(nside, i, vec);
+
+        printf("dot%d = translate([%f,%f,%f])(sphere(%f, segments=20))\n", i, vec[0]*sphere_radius, vec[1]*sphere_radius, vec[2]*sphere_radius, dot_radius);
+
+        if (i==0){
+            printf("scene = dot0\n");
+        }
+        else{
+            printf("scene += dot%d\n", i);
+        }
+    }
+
+    printf("print(scad_render(scene))\n");
 }
+
+
