9ecf85adbd177b48a1363018159718dea7685696
Tom Verbeure <hombre+github@gmail.com>
2019-09-07 21:08:45 -0700

    Polyhedron fail.

--- healpix/src/healball.c
+++ healpix/src/healball.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
     int npix = nside2npix(nside);
 
     float dot_radius = 0.1;
-    float sphere_radius = 5;
+    float sphere_radius = 10;
 
     printf("# npix: %d\n", npix);
     printf("from solid import *\n\n");
@@ -62,9 +62,36 @@ int main(int argc, char **argv)
         }
     }
 
-    //printf("scene += color(Red)(polyhedron(points=[dot_coords[0], dot_coords[1], dot_coords[3]], faces=[ [0,1,2] ]))\n");
 
-    printf("scene += color(Green)(sphere(%f,segments=40))\n", sphere_radius-0.2);
+    double boundary_vec0[3];
+    pix2vec_nest(nside*4, xyf2nest(nside*4, 0, 0, 0), boundary_vec0);
+
+    double boundary_vec1[3];
+    pix2vec_nest(nside*4, xyf2nest(nside*4, nside*4-1, 0, 0), boundary_vec1);
+
+    double boundary_vec2[3];
+    pix2vec_nest(nside*4, xyf2nest(nside*4, nside*4-1, nside*4-1, 0), boundary_vec2);
+
+    double boundary_vec3[3];
+    pix2vec_nest(nside*4, xyf2nest(nside*4, 0, nside*4-1, 0), boundary_vec3);
+
+    printf("boundary_vecs = [ [0,0,0] ]\n");    
+    double boundary_radius = sphere_radius * 1.5;
+    printf("boundary_vecs.append([%f,%f,%f])\n", boundary_vec0[0] * boundary_radius, boundary_vec0[1] * boundary_radius, boundary_vec0[2] * boundary_radius);
+    printf("boundary_vecs.append([%f,%f,%f])\n", boundary_vec1[0] * boundary_radius, boundary_vec1[1] * boundary_radius, boundary_vec1[2] * boundary_radius);
+    printf("boundary_vecs.append([%f,%f,%f])\n", boundary_vec2[0] * boundary_radius, boundary_vec2[1] * boundary_radius, boundary_vec2[2] * boundary_radius);
+    printf("boundary_vecs.append([%f,%f,%f])\n", boundary_vec3[0] * boundary_radius, boundary_vec3[1] * boundary_radius, boundary_vec3[2] * boundary_radius);
+
+    printf("scene += color(Magenta)(translate(boundary_vecs[1])(sphere(%f, segments=10)))\n", dot_radius);
+    printf("scene += color(Magenta)(translate(boundary_vecs[2])(sphere(%f, segments=10)))\n", dot_radius);
+    printf("scene += color(Magenta)(translate(boundary_vecs[3])(sphere(%f, segments=10)))\n", dot_radius);
+    printf("scene += color(Magenta)(translate(boundary_vecs[4])(sphere(%f, segments=10)))\n", dot_radius);
+
+    printf("scene += color(Yellow)(polyhedron(points=[dot_coords[0], dot_coords[1], dot_coords[3]], faces=[ [0,1,2] ]))\n");
+
+    printf("scene += color(Green)(sphere(%f,segments=40))\n", sphere_radius-1.0);
+
+    printf("scene += color(Yellow)(polyhedron(points=boundary_vecs, faces=[ [0,1,2], [0,2,3], [0,3,4], [0,4,1], [1,2,4], [3,4,2] ]))\n");
 
     printf("print(scad_render(scene))\n");
 }
